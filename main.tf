a9e9221ebde7b69e93e2ddde76c9fc515884dfff
Ramachandra Nayak <rama@Ramachandras-MacBook-Air.local>
2024-05-16 14:35:38 +0530

    added output and provider file in the terraform, output file is empty

--- main.tf
+++ main.tf
@@ -1,22 +1,9 @@
-terraform{
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 5.0"
-        }
-    }
-    required_version = ">= 1.2.0"
-}
-
-provider "aws" {
-    region = "us-east-1"
-}
-
 resource "aws_instance" "web" {
-  ami = "${var.OS}"
-  instance_type = "${var.Processor}"
-  key_name = "${var.KeyPair}"
-  vpc_security_group_ids = ["${var.SecGroupId}"]
+  ami = var.OS
+  instance_type = var.Processor
+  key_name = var.KeyPair
+  vpc_security_group_ids = [ var.SecGroupId ]
+  provider = aws.us-east-1
   tags = {
     Name = "terraform_Creation"
   }
